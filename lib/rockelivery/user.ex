d8dff3c1ac5675f133f62ec41b8a68d3a51f7712
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-09 11:59:55 -0300

    Criando o schema do User pt 2

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -3,9 +3,11 @@ defmodule Rockelivery.User do
 
   import Ecto.Changeset
 
+  alias Ecto.Changeset
+
   @primary_key {:id, :binary_id, autogenerate: true}
 
-  @required_params [:address, :age, :cep, :cpf, :email, :password_hash, :name]
+  @required_params [:address, :age, :cep, :cpf, :email, :password, :name]
 
   schema "users" do
     field :address, :string
@@ -13,6 +15,7 @@ defmodule Rockelivery.User do
     field :cep, :string
     field :cpf, :string
     field :email, :string
+    field :password, :string, virtual: true
     field :password_hash, :string
     field :name, :string
 
@@ -22,5 +25,20 @@ defmodule Rockelivery.User do
   def changeset(params) do
     %__MODULE__{}
     |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
 end
