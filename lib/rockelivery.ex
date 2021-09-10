b6048a6440c89e036746fe513068735552c8bf13
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-10 13:33:09 -0300

    Criando a rota de show dos usuários

--- lib/rockelivery.ex
+++ lib/rockelivery.ex
@@ -1,5 +1,7 @@
 defmodule Rockelivery do
   alias Rockelivery.User.Create, as: UserCreate
+  alias Rockelivery.User.Get, as: UserGet
 
   defdelegate create_user(params), to: UserCreate, as: :call
+  defdelegate get_user_by_id(id), to: UserGet, as: :by_id
 end
