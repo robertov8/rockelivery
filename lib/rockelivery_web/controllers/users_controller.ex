0ff7f8ee4c055f483b5017e7ef125246cddaeed1
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-10 15:41:18 -0300

    Criando a rota de deleção de usuários

--- lib/rockelivery_web/controllers/users_controller.ex
+++ lib/rockelivery_web/controllers/users_controller.ex
@@ -21,4 +21,12 @@ defmodule RockeliveryWeb.UsersController do
       |> render("show.json", user: user)
     end
   end
+
+  def delete(conn, %{"id" => id}) do
+    with {:ok, %User{}} <- Rockelivery.delete_user(id) do
+      conn
+      |> put_status(:no_content)
+      |> text("")
+    end
+  end
 end
