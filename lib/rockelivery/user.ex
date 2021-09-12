d9674500cc0a24ef1a218088cec968d0e8f6630d
Roberto Ribeiro <robertov82008@gmail.com>
2021-09-12 12:47:36 -0300

    Evitando chamadas desnecessárias para a API

--- lib/rockelivery/user.ex
+++ lib/rockelivery/user.ex
@@ -27,6 +27,8 @@ defmodule Rockelivery.User do
     timestamps()
   end
 
+  def build(changeset), do: apply_action(changeset, :create)
+
   def changeset(struct \\ %__MODULE__{}, params) do
     struct
     |> cast(params, @required_params)
--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -1,16 +1,17 @@
 defmodule Rockelivery.Users.Create do
   alias Rockelivery.{Error, Repo, User}
+  alias Rockelivery.ViaCep.Client
 
-  def call(params) do
-    params
-    |> User.changeset()
-    |> Repo.insert()
-    |> handle_insert()
-  end
+  def call(%{"cep" => cep} = params) do
+    changeset = User.changeset(params)
 
-  defp handle_insert({:ok, %User{}} = result), do: result
-
-  defp handle_insert({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
+    with {:ok, %User{}} <- User.build(changeset),
+         {:ok, _cep_info} <- Client.get_cep_info(cep),
+         {:ok, %User{}} = user <- Repo.insert(changeset) do
+      user
+    else
+      {:error, %Error{}} = error -> error
+      {:error, reason} -> {:error, Error.build(:bad_request, reason)}
+    end
   end
 end
