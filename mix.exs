c5f8f83036db00128e866029cb9964438c907bbe
Roberto Ribeiro <robertoribeiro.code@gmail.com>
2021-09-15 21:45:36 -0300

    Criando o teste do nosso client pt 1

--- mix.exs
+++ mix.exs
@@ -55,7 +55,8 @@ defmodule Rockelivery.MixProject do
       {:excoveralls, "~> 0.10", only: :test},
       {:ex_machina, "~> 2.7.0"},
       {:tesla, "~> 1.4.3"},
-      {:hackney, "~> 1.17.4"}
+      {:hackney, "~> 1.17.4"},
+      {:bypass, "~> 2.1.0", only: :test}
     ]
   end
 
