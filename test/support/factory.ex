034f8aa3f58d21d514a3e7189e7605e533eb0e31
Roberto Ribeiro <robertoribeiro.code@gmail.com>
2021-09-23 09:57:18 -0300

    Corrigindo os testes da criação do User pt 2

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -27,4 +27,19 @@ defmodule Rockelivery.Factory do
       name: "Roberto Ribeiro"
     }
   end
+
+  def cep_info_factory do
+    %{
+      "bairro" => "Sé",
+      "cep" => "01001-000",
+      "complemento" => "lado ímpar",
+      "ddd" => "11",
+      "gia" => "1004",
+      "ibge" => "3550308",
+      "localidade" => "São Paulo",
+      "logradouro" => "Praça da Sé",
+      "siafi" => "7107",
+      "uf" => "SP"
+    }
+  end
 end
