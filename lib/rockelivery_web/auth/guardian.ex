2081d21b9cd68b6d011d67d1045549ff92093dbb
Roberto Ribeiro <robertoribeiro.code@gmail.com>
2021-09-25 15:36:57 -0300

    Validando o usuário e senha

--- lib/rockelivery_web/auth/guardian.ex
+++ lib/rockelivery_web/auth/guardian.ex
@@ -1,10 +1,23 @@
 defmodule RockeliveryWeb.Auth.Guardian do
   use Guardian, otp_app: :rockelivery
 
-  alias Rockelivery.User
+  alias Rockelivery.{Error, User}
   alias Rockelivery.Users.Get, as: UserGet
 
   def subject_for_token(%User{id: id}, _claims), do: {:ok, id}
 
   def resource_from_claims(%{"sub" => id}), do: UserGet.by_id(id)
+
+  def authenticate(%{"id" => user_id, "password" => password}) do
+    with {:ok, %User{password_hash: hash} = user} <- UserGet.by_id(user_id),
+         true <- Pbkdf2.verify_pass(password, hash),
+         {:ok, token, _claims} <- encode_and_sign(user) do
+      {:ok, token}
+    else
+      false -> {:error, Error.build(:unauthorized, "Please verify yout credentials")}
+      error -> error
+    end
+  end
+
+  def authenticate(_), do: {:error, Error.build(:bad_request, "Please verify yout credentials")}
 end
